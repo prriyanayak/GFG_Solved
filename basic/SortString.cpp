4ffd51ecdba2250eda63afa634eb7673afe3137b
Priya Nayak <nayakpriya98@gmail.com>
2019-05-22 11:42:28 +0530

    Create SortString.cpp

---
+++ basic/SortString.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	//code
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    string s;
+	    cin >> s;
+	    sort(s.begin(), s.end());
+	    cout << s <<endl;
+	    
+	}
+	return 0;
+}
