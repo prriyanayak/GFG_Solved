8006e19548f3abebba3acf0b6b243f2d0cf43654
Priya Nayak <nayakpriya98@gmail.com>
2019-05-22 12:19:13 +0530

    Create AnshumansFavNumber.cpp

---
+++ basic/AnshumansFavNumber.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long int n;
+        cin >> n;
+        if (n%5 == 0)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+	//code
+	return 0;
+}
