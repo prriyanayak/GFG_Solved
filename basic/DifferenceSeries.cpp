24243bffd40dbb058d739ce835099f1013a47c97
Priya Nayak <nayakpriya98@gmail.com>
2019-05-22 13:08:36 +0530

    Create DifferenceSeries.cpp

---
+++ basic/DifferenceSeries.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	//code
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    cout << n*((2*n)+1) << endl;
+	}
+	return 0;
+}
