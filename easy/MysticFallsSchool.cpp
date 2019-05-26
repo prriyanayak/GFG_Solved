da3d6bcc7576497900c55c5a9b408abb44de5707
Priya Nayak <nayakpriya98@gmail.com>
2019-05-26 19:31:10 +0530

    Create MysticFallsSchool.cpp

---
+++ easy/MysticFallsSchool.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n, k;
+	    cin >> n >> k;
+	    cout << (k-n-1) << endl;
+	}
+	return 0;
+}
