6cb4ce7c4701ea0f154184862addfe17171c3f56
Priya Nayak <nayakpriya98@gmail.com>
2019-05-27 23:52:21 +0530

    Update BinarySearch.cpp

--- basic/BinarySearch.cpp
+++ basic/BinarySearch.cpp
@@ -1,15 +1,13 @@
 int bin_search(int A[], int left, int right, int k)
 {
-    int mid;
-    while(left <= right)
-    {
-        mid = (left + right)/2;
-        if (A[mid] == k)
-            return mid;
-        else if (A[mid] < k)
-            left = mid + 1;
-        else
-            right = mid + 1;
-    }
-    return -1;
+    int mid = (left + right)/2;
+    if(left > right)
+        return -1;
+
+    else if (A[mid] == k)
+        return mid;
+    else if (A[mid] < k)
+        return bin_search(A, mid+1, right, k);
+    else
+        return bin_search(A, left, mid-1, k);
 }
