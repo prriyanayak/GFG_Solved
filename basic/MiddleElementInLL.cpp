5c23b029313cc757d8b71e4fad6f00c0bd2951c7
Priya Nayak <nayakpriya98@gmail.com>
2019-05-27 21:00:20 +0530

    Create MiddleElementInLL.cpp

---
+++ basic/MiddleElementInLL.cpp
@@ -0,0 +1,12 @@
+int getMiddle(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow->data;
+}
