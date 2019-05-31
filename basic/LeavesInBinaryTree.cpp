2fb0724f8785cbd9098de905892b7545a7e97da9
Priya Nayak <nayakpriya98@gmail.com>
2019-05-31 12:58:53 +0530

    Create LeavesInBinaryTree.cpp

---
+++ basic/LeavesInBinaryTree.cpp
@@ -0,0 +1,8 @@
+int countLeaves(Node* root)
+{
+    if (root == NULL)
+        return 0;
+    if(root->left == NULL && root->right == NULL)
+        return 1;
+    return (countLeaves(root->left) + countLeaves(root->right));
+}
