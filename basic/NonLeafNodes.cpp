af167ca14f7bebaeafe137f37ca277fb66c3443a
Priya Nayak <nayakpriya98@gmail.com>
2019-05-31 13:07:40 +0530

    Create NonLeafNodes.cpp

---
+++ basic/NonLeafNodes.cpp
@@ -0,0 +1,6 @@
+int countNonLeafNodes(Node* root)
+{
+    if (root == NULL || (root->left == NULL && root->right == NULL))
+        return 0;
+    return 1 + countNonLeafNodes(root->left) + countNonLeafNodes(root->right);
+}
