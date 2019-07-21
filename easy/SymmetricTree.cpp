d721539f78b6437062931dd89302bb382f656f94
Priya Nayak <nayakpriya98@gmail.com>
2019-07-21 19:29:22 +0530

    Create SymmetricTree.cpp

---
+++ easy/SymmetricTree.cpp
@@ -0,0 +1,12 @@
+bool isMirror(struct Node* root1, struct Node* root2)
+{
+    if (root1 == NULL && root2 == NULL)
+        return true;
+    if (root1 != NULL && root2 != NULL && root1->key == root2->key)
+        return isMirror(root1->left, root2->right) && isMirror(root1->right, root2->left);
+    return false;
+}
+bool isSymmetric(struct Node* root)
+{   
+    return isMirror(root, root);
+}
